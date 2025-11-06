5e447d44a48e1c8fc28dd6a1f8c783eb435990c6
Bibin T <bibint2918@gmail.com>
2025-11-06 16:11:28 +0530

    Revert "fix/make answer aand source fields empty"
    This reverts commit f60802f5da68a7b844cd5d3b668957f7545d9ced.

--- frontend/src/components/QA-interface.tsx
+++ frontend/src/components/QA-interface.tsx
@@ -291,8 +291,7 @@ export const QAInterface = () => {
     rejectionReason?: string
   ) => {
     if (!selectedQuestion || isResponding) return;
-    setNewAnswer('')
-    setSources([])
+
     let payload = { questionId: selectedQuestion } as IReviewAnswerPayload;
 
     if (!status) {
