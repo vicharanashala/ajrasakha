16429b5f99257993532cb213ba911da9bc839334
Bibin T <bibint2918@gmail.com>
2025-12-22 12:22:44 +0530

    chore: added spinner for question details page

--- frontend/src/components/questions-page.tsx
+++ frontend/src/components/questions-page.tsx
@@ -16,6 +16,7 @@ import {
 import { useDebounce } from "@/hooks/ui/useDebounce";
 import { useBulkDeleteQuestions } from "@/hooks/api/question/useBulkDeleteQuestions";
 import { toast } from "sonner";
+import Spinner from "./atoms/spinner";
 
 export const QuestionsPage = ({
   currentUser,
@@ -189,18 +190,23 @@ export const QuestionsPage = ({
 
   return (
     <main className={"mx-auto w-full p-4 md:p-6 space-y-6"}>
-      {selectedQuestionId && questionDetails ? (
-        <>
-          <QuestionDetails
-            question={questionDetails.data}
-            currentUserId={questionDetails.currentUserId}
-            refetchAnswers={refechSelectedQuestion}
-            isRefetching={isLoadingSelectedQuestion}
-            // goBack={() => setSelectedQuestionId("")}
-            goBack={goBack}
-            currentUser={currentUser!}
-          />
-        </>
+      {selectedQuestionId ? (
+    isLoadingSelectedQuestion ? (
+      <div className="flex items-center justify-center min-h-[60vh]">
+        <Spinner />
+      </div>
+    ) : (
+      questionDetails && (
+        <QuestionDetails
+          question={questionDetails.data}
+          currentUserId={questionDetails.currentUserId}
+          refetchAnswers={refechSelectedQuestion}
+          isRefetching={isLoadingSelectedQuestion}
+          goBack={goBack}
+          currentUser={currentUser!}
+        />
+      )
+    )
       ) : (
         <>
           <QuestionsFilters
