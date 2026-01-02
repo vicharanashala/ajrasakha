f7036920eb1c949e62a09488c0a2ad953aced192
Abiram-k <abiramk0107@gmail.com>
2026-01-02 10:57:50 +0530

    chore: remove commented codes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,51 +4,8 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import "./styles.css";
 import reportWebVitals from "./reportWebVitals.ts";
-import { worker } from "./mocks/browser.js";
 import { client } from "./client/client.gen.ts";
 import { env } from "./config/env.ts";
-// import { useAuthStore } from "./stores/auth-store.ts";
-
-// if (env.enableMocks() === true) {
-//   await worker.start();
-// }
-// client.setConfig({
-//   baseUrl: env.apiBaseUrl() || "http://localhost:4000",
-//   // auth: `${useAuthStore.getState().token}`,
-// });
-
-// // Create a new router instance
-// const router = createRouter({
-//   routeTree,
-//   context: {},
-//   defaultPreload: "intent",
-//   scrollRestoration: true,
-//   defaultStructuralSharing: true,
-//   defaultPreloadStaleTime: 0,
-// });
-
-// // Register the router instance for type safety
-// declare module "@tanstack/react-router" {
-//   interface Register {
-//     router: typeof router;
-//   }
-// }
-
-// // Render the app
-// const rootElement = document.getElementById("app");
-// if (rootElement && !rootElement.innerHTML) {
-//   const root = ReactDOM.createRoot(rootElement);
-//   root.render(
-//     <StrictMode>
-//       <RouterProvider router={router} />
-//     </StrictMode>
-//   );
-// }
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 async function bootstrap() {
   // Enable MSW only when explicitly allowed
