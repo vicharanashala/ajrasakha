96a51d63e5f256a129c2ccf4dce702ed03f8d132
Thanha Mariyam <thanhathanha665@gmail.com>
2026-01-22 17:40:06 +0530

    fix:update check-in button to reflect status immediately

--- frontend/src/hooks/api/performance/useCheckIn.ts
+++ frontend/src/hooks/api/performance/useCheckIn.ts
@@ -1,13 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient     } from "@tanstack/react-query";
 import { PerformaneService } from "../../services/performanceService";
 
 const performaceService = new PerformaneService();
 
 export const useCheckIn = () => {
+    const queryClient = useQueryClient();
     const { mutateAsync, isPending, error, data } = useMutation({
         mutationFn: async () => {
             return await performaceService.checkIn();
         },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["experts"] });
+            queryClient.invalidateQueries({ queryKey: ["currentUser"] });
+    },
     });
 
     return { checkIn: mutateAsync, isPending, error, data };
