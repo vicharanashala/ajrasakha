596b7c931593f41dbcbc4114c3f24bd9b5a28cd1
Thanha Mariyam <thanhathanha665@gmail.com>
2026-01-30 17:43:54 +0530

    fix(comment): return only required fields in API responses

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -124,21 +124,21 @@ export interface HistoryItem {
   // If an expert is modifying, it store the modified answer id
   modifiedAnswer?: string;
   // timestamp
- 
+
   moderator?: ModeratorRerouteRepo;
   question?: QuestionEntityRerouteRepo;
- 
+
   rerouteId?: string;
   reroute?: RerouteRerouteRepo;
   text?: string;
-  
+
   details?: QuestionDetailsRerouteRepo;
   createdAt?: Date;
   priority?: Priority;
   id?: string;
- 
+
   updatedAt?:Date,
- 
+
 }
 
 export type QuestionPriority = "low" | "medium" | "high";
@@ -425,9 +425,6 @@ export interface QuestionFullDataResponse {
 
 export interface IComment {
   _id: string;
-  questionId: string;
-  answerId: string;
-  userId: string;
   userName?: string;
   text: string;
   createdAt: string;
@@ -594,7 +591,7 @@ export interface IRerouteHistoryResponse {
 export type RerouteHistoryApiResponse = IRerouteHistoryResponse[];
 type Priority = "high" | "medium" | "low";
 
- export interface ReroutedQuestionItem {
+export interface ReroutedQuestionItem {
   id: string;
   text: string;
   status: QuestionStatus;
@@ -692,7 +689,7 @@ export interface QuestionHistoryRerouteRepo {
   createdAt?: string;
   priority?: Priority;
   id?: string;
- 
+
   updatedAt?:Date,
   updatedBy?: {
     // who's submission is this
@@ -700,8 +697,8 @@ export interface QuestionHistoryRerouteRepo {
     userName: string;
     // email: string;
   };
-  
-  
+
+
 }
 
 /* =========================
@@ -788,15 +785,15 @@ export interface RerouteRerouteRepo {
 }
 export type QuestionResponse =
   | {
-      kind: "normal";
-      data: IQuestion;
-    }
+    kind: "normal";
+    data: IQuestion;
+  }
   | {
-      kind: "reroute";
-      data: QuestionRerouteRepo;
-    };
-    export interface WorkloadBalanceResponse {
-      message: string;
-      expertsInvolved: number;
-      submissionsProcessed: number;
-    }
+    kind: "reroute";
+    data: QuestionRerouteRepo;
+  };
+export interface WorkloadBalanceResponse {
+  message: string;
+  expertsInvolved: number;
+  submissionsProcessed: number;
+}
