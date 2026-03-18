c214e10ac38f26fdbe1cd912feb1a3b1bfe1175b
Anjaligalav <anjaligalav294@gmail.com>
2026-03-18 13:49:15 +0530

    remove duplicate question when question got delete

--- backend/src/modules/question/services/QuestionService.ts
+++ backend/src/modules/question/services/QuestionService.ts
@@ -2105,6 +2105,9 @@ export class QuestionService extends BaseService implements IQuestionService {
       );
       await this.requestRepository.deleteByEntityId(questionId, activeSession);
 
+      // Delete duplicate question records referencing this question
+      await this.duplicateQuestionRepository.deleteByReferenceQuestionId(questionId, activeSession);
+
       // Finally, delete the question itself
       return this.questionRepo.deleteQuestion(questionId, activeSession);
     };
