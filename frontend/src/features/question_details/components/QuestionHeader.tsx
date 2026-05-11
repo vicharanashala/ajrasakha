28431d0c53555bc403efca770445164b3b0570a6
vishwas-mehta <vishwas.mehta.work@gmail.com>
2026-05-11 13:21:07 +0530

    fix: changed the badge name from uppercase to lowercase

--- frontend/src/features/question_details/components/QuestionHeader.tsx
+++ frontend/src/features/question_details/components/QuestionHeader.tsx
@@ -175,7 +175,7 @@ export const QuestionHeader = ({ question, goBack, currentUser,isQuestionAllocat
                 <Badge
                   className="bg-red-400/10 text-red-500 border-red-400/30 hover:bg-red-400/20 transition-colors"
                 >
-                  DUPLICATE
+                  Duplicate
                 </Badge>
               </button>
             )
