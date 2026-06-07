5be37007577e7c29c7ee17d85ca415b8c4fa9b48
afnad ca <afnadca2@gmail.com>
2026-06-07 10:43:28 +0530

    refactor: update sonner toast to custom toast in delete and bulkdelete questions

--- frontend/src/hooks/api/question/useDeleteQuestion.ts
+++ frontend/src/hooks/api/question/useDeleteQuestion.ts
@@ -1,23 +1,33 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { QuestionService } from "../../services/questionService";
-import {toast} from "sonner";
+import { useToast } from "@/shared/components/toast";
 
 const questionService = new QuestionService();
 
 export const useDeleteQuestion = () => {
+  const {loading: toastLoading, success: toastSuccess, error: toastError, dismiss: toastDismiss} = useToast()
   const queryClient = useQueryClient();
 
   return useMutation({
+    onMutate: ()=> {
+      const toastId = toastLoading('deleting the question...',{
+        desc: "please wait while deleting the question"
+      })
+
+      return { toastId }
+    },
     mutationKey: ["deleteQuestion"],
     mutationFn: async (questionId: string): Promise<void> => {
       await questionService.deleteQuestion(questionId);
     },
-    onSuccess: () => {
-      toast.success("Question deleted successfully");
+    onSuccess: (_,__,context) => {
+      if(context?.toastId)toastDismiss(context.toastId)
+      toastSuccess("Question deleted successfully");
       queryClient.invalidateQueries({ queryKey: ["detailed_questions"] });
     },
-    onError: (error: any) => {
-      toast.error("Failed to delete question");
+    onError: (error: any,_,context) => {
+      if(context?.toastId)toastDismiss(context.toastId)
+      toastError("Failed to delete question");
       console.error("Delete question error:", error);
     },
   });
