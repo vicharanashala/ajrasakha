021eade8863ce5119a6122f4d198fa1d4ddc3da2
Thanha Mariyam <thanhathanha665@gmail.com>
2026-06-23 15:38:37 +0530

    feat:bulk notification and push notification

--- backend/src/shared/interfaces/models.ts
+++ backend/src/shared/interfaces/models.ts
@@ -291,6 +291,7 @@ export type INotificationType =
   | 'delayed_question'
   | 'moderator_approval'
   | 'allocation_removal'
+  | 'coordinator_message';
 export interface INotification {
   _id?: string | ObjectId;
   userId: string | ObjectId;
