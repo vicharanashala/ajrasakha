399b37aa3afcf942c02a72395a413fade7c09853
Sreehari VR <vrsreehari61@gmail.com>
2026-07-08 11:27:57 +0530

    fix:today query search is not working

--- backend/src/shared/database/providers/mongo/repositories/ChatbotRepository.ts
+++ backend/src/shared/database/providers/mongo/repositories/ChatbotRepository.ts
@@ -14618,11 +14618,15 @@ export class ChatbotRepository implements IChatbotRepository {
           .project({_id: 1})
           .toArray();
 
-        const searchUserIds = matchingUsers.map(user => user._id);
+        const searchUserIds = matchingUsers.map(user => user._id.toString());
 
-        matchQuery.user = {
-          $in: searchUserIds,
-        };
+        if (matchQuery.user && matchQuery.user.$in) {
+          matchQuery.user.$in = matchQuery.user.$in.filter((id: string) => searchUserIds.includes(id));
+        } else {
+          matchQuery.user = {
+            $in: searchUserIds,
+          };
+        }
       }
 
       const result = await this.messagesCollection
