9e44a9e3f209a777cc52fabad34b51a12e12aaa3
User <user@example.com>
2026-07-14 15:36:37 +0530

    fix: removed consoles from start file

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -36,20 +36,6 @@ echo "Running tailscale up..."
   --auth-key="${TAILSCALE_AUTHKEY}" \
   --hostname=gcp
 
-echo "========== Tailscale Status =========="
-/app/tailscale status
-
-echo "========== Tailscale IP =========="
-/app/tailscale ip
-
-echo "========== Ping Target =========="
-/app/tailscale ping 100.100.108.44 || true
-echo ""
-echo "========== HTTP TEST =========="
-
-curl -v http://100.100.108.44:2026/threads/5f0ba437-c68d-4a54-97c9-138752acf4d6/state
-
-echo "========== END HTTP TEST =========="
 echo "Starting Node application..."
 
 exec dumb-init node build/index.js
