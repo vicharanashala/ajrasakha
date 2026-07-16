1a1941e3fc33dc179840ced34c280f33937391a7
mamatha12-reddy <mamathareddy3456.reddeppa@gmail.com>
2026-07-16 11:42:13 +0530

    commented cron jobs

--- backend/src/bootstrap/jobs/index.ts
+++ backend/src/bootstrap/jobs/index.ts
@@ -1,12 +1,12 @@
-import './questionStatus.js';
-import './notificationDelete.js'
-import './backupDB.js'
-import './dailyReport.js'
-// import './absentCron.js'
-import './reAllocateCron.js'
-import './timeBoundReAllocateCron.js'
-import './moderatorQueueCron.js'
-//import './embeddingBackfill.js'
+//import './questionStatus.js';
+//import './notificationDelete.js'
+//import './backupDB.js'
+//import './dailyReport.js'
+// import './absentCron.js'//this is commented earlier
+//import './reAllocateCron.js'
+//import './timeBoundReAllocateCron.js'
+//import './moderatorQueueCron.js'
+//import './embeddingBackfill.js'//this is commented earlier
 export const initJobs = () => {
   console.log('[CRON] Jobs initialized.');
 };
