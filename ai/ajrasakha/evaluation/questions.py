75bf66d776ac1e5581468375ecd920aa315afe82
Mohamed Asif <24f1001713@ds.study.iitm.ac.in>
2026-07-24 12:01:45 +0530

    Add multilingual government scheme evaluation test cases

--- ai/ajrasakha/evaluation/questions.py
+++ ai/ajrasakha/evaluation/questions.py
@@ -144,6 +144,8 @@ TEST_CASES = [
             
             "crop": "all",
             "is_complete": True,
+            "script_language": "English",
+            "vocal_language": "English",
         },
     },
     {
@@ -160,6 +162,8 @@ TEST_CASES = [
           
             "crop": "all",
             "is_complete": True,
+            "script_language": "English",
+            "vocal_language": "English",
         },
     },
     {
