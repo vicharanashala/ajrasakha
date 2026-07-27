9efc62858f7e343a8ee48b17130acc9d52d5faf8
mamatha12-reddy <mamathareddy3456.reddeppa@gmail.com>
2026-07-27 10:15:41 +0530

    added cron jobs back in production

--- backend/src/bootstrap/jobs/index.ts
+++ backend/src/bootstrap/jobs/index.ts
@@ -1,13 +1,13 @@
-//import './questionStatus.js';
-//import './notificationDelete.js'
-//import './backupDB.js'
-//import './dailyReport.js'
+import './questionStatus.js';
+import './notificationDelete.js'
+import './backupDB.js'
+import './dailyReport.js'
 // import './absentCron.js'//previously commented
-//import './reAllocateCron.js'
-//import './timeBoundReAllocateCron.js'
-//import './moderatorQueueCron.js'
-//import './agentStatusCleanupJob.js';
-//import './gateKeeperAuditorQueueCron.js'
+import './reAllocateCron.js'
+import './timeBoundReAllocateCron.js'
+import './moderatorQueueCron.js'
+import './agentStatusCleanupJob.js';
+import './gateKeeperAuditorQueueCron.js'
 //import './embeddingBackfill.js'//previously commented
 export const initJobs = () => {
   console.log('[CRON] Jobs initialized.');
