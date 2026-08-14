79c7ed8d3d1416970553d45139e1f6ff2e0a8599
mamatha12-reddy <mamathareddy3456.reddeppa@gmail.com>
2026-08-14 17:26:17 +0530

    hidden public dashbaord page to enable endpoints

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,28 @@
-import { createFileRoute } from "@tanstack/react-router";
+//To make public dashboard 
+
+/*import { createFileRoute } from "@tanstack/react-router";
 import { HomeDashboard } from "@/features/home-dashboard/HomeDashboard";
 
 export const Route = createFileRoute("/")({
   component: HomeDashboard,
+});*/
+
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useAuthStore } from "@/stores/auth-store";
+import { useEffect } from "react";
+
+export const Route = createFileRoute("/")({
+  component: App,
 });
+
+function App() {
+  const { initAuthListener } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    initAuthListener();
+    navigate({ to: "/home" });
+  }, [initAuthListener]);
+
+  return <></>;
+}
