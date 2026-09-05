cfb1f7924c3d8f9eda06cfcef258585f4cc8152c
afnad ca <afnadca2@gmail.com>
2026-09-05 16:50:04 +0530

    feat(frontend): update toast in edit user details dialog

--- frontend/src/hooks/api/Admin/useAdminEditUser.ts
+++ frontend/src/hooks/api/Admin/useAdminEditUser.ts
@@ -1,7 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "sonner";
+
 import { AdminUserService } from "@/hooks/services/adminService";
 import type { IUser, IUserAdminEdit } from "@/types";
+import { toast } from "@/shared/components/toast";
 
 const adminUserService = new AdminUserService();
 
@@ -19,7 +20,11 @@ export const useAdminEditUser = () => {
     }): Promise<IUser | null> => {
       return adminUserService.editUserDetails(userId, data);
     },
-    onSuccess: () => {
+    onMutate: () =>{
+      const toastId = toast.loading('Updating user details...')
+      return {toastId}
+    },
+    onSuccess: (_,__,context) => {
       queryClient.invalidateQueries({
         queryKey: ["admin"],
         exact: false,
@@ -36,9 +41,11 @@ export const useAdminEditUser = () => {
         queryKey: ["userReviewLevel"],
         exact: false,
       });
+      if(context?.toastId)toast.dismiss(context.toastId)
       toast.success("User details updated successfully!");
     },
-    onError: (error: Error) => {
+    onError: (error: Error,_,context) => {
+      if(context?.toastId)toast.dismiss(context.toastId)
       toast.error(
         error?.message || "Failed to update user details. Please try again.",
       );
